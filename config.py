bf006bc3763deb1b49640b233483c71102015bec
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2023-09-06 06:56:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from telethon import TelegramClient
-api_id = 23442389
-api_hash = "70490ec8a810932cb5cb7f9d6a839ee0"
-bot_token = "6107533276:AAHR4lKUnLGFvR6cpnUzjzvUCqNzLd69rZE"
+api_id = 21080577
+api_hash = "23bf3f7e84830378c419effeef1624e3"
+bot_token = "6469469309:AAGdLA-o5h975CGAG8giGtCPfnzrq1HpsO8"
 skeleton_url = ""
 
 api_id = os.environ.get('API_ID')
